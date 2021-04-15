ff2dce4be48c75edbcacf6aea588e6617c07333a
MailineN <nguyen.mailine@gmail.com>
2021-04-15 10:44:08 +0200

    add api fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Liste from "./Liste";
-import data from "./data.json";
+//import data from "./data.json";
 
 function App() {
+  const [data, setDataState] = useState(undefined);
+  const URL = 'https://api.covid19api.com/summary';
+  useEffect(() => {
+    if (!data) {
+      fetch(URL)
+        .then((res) => res.json())
+        .then((json) => setDataState(json));
+    }
+  });
   return (
     <div className="App">
       <header className="App-header">
